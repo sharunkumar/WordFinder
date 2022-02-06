d9aab8260fa4ad5f3fdc12ae8c46fc4bf4295563
Sharun <sharunksplus@gmail.com>
2022-02-06 15:25:43 +0530

    removed the limit parameter to get all the words using recursion

--- WordFinder.ps1
+++ WordFinder.ps1
@@ -5,13 +5,23 @@ param (
     , [Parameter()] [string] $Contains
     , [Parameter()] [string] $Exclude
     , [Parameter()] [int] $WordLength = 5
-    , [Parameter()] [int] $Limit = 2500
 )
 
+function Get-WordsApi($Offset = 0) {
+    $Limit = 2500
+    $webResult = Invoke-WebRequest -Uri "https://api.yourdictionary.com/wordfinder/v1/wordlist?special=length&order_by=alpha&limit=$Limit&offset=$Offset&word_length=$WordLength&starts=$StartsWith&contains=$($Contains[0])&ends=$EndsWith&has_definition=check&suggest_links=true&dictionary=WL"
+    $jsonData = ($webResult.Content | ConvertFrom-Json).data
+    $words = $jsonData._items
+    $total = $jsonData._meta.total
 
-$webResult = Invoke-WebRequest -Uri "https://api.yourdictionary.com/wordfinder/v1/wordlist?special=length&order_by=alpha&limit=$Limit&offset=0&word_length=$WordLength&starts=$StartsWith&contains=$($Contains[0])&ends=$EndsWith&has_definition=check&suggest_links=true&dictionary=WL"
+    if (($words.Length + $Offset) -lt $total) {
+        return $words + (Get-WordsApi($Offset + $words.Length))
+    }
 
-$words = ($webResult.Content | ConvertFrom-Json).data._items
+    return $words
+}
+
+$words = Get-WordsApi
 
 if ($Contains.Length -gt 1) {
     for ($i = 1; $i -lt $Contains.Length; $i++) {
